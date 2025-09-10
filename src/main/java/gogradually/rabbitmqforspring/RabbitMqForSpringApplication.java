e0b660efa16e81a499092b8a13f7e435e3a52a8a
GoGradually <chkun3109@gmail.com>
2025-09-10 14:11:16 +0900

    initial commit

---
+++ src/main/java/gogradually/rabbitmqforspring/RabbitMqForSpringApplication.java
@@ -0,0 +1,13 @@
+package gogradually.rabbitmqforspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqForSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqForSpringApplication.class, args);
+    }
+
+}
